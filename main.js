dca1e0ea9370d6c750e79fa6dcf2edf2189dc5f2
Yasmin Verderame <yasmin_verderame@hotmail.com>
2022-10-17 16:48:23 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+"use strict";
+const slides = [
+  { 
+    className: '.slider-customers', 
+    curSlide: 0
+  },
+  { 
+    className: '.slider-video', 
+    curSlide: 0
+  },
+  { 
+    className: '.slider-why-answer-by-whatsapp', 
+    curSlide: 0
+  },
+  { 
+    className: '.slider-plans', 
+    curSlide: 0
+  }
+];
